b1e9de4c0ae2ccc77dab2650c5471b0f27a5b76e
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-05-17 01:00:54 +0330

    add the route post for the border

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,15 @@
 <?php
 
+use App\Models\Board;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
+});
+
+Route::post('board', function(Request $request) {
+    Board::Create($request->all());
 });
 
 require __DIR__.'/auth.php';
