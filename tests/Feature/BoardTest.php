fc071424533e19588410740358980bd1e1fdd999
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-05-17 05:09:24 +0330

    test (access) : create user and fix the post to post Json

--- tests/Feature/BoardTest.php
+++ tests/Feature/BoardTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -12,7 +13,10 @@ class BoardTest extends TestCase
     /** @test */
     public function user_can_create_a_board(): void
     {
-        $response = $this->post('/board', [
+        $user = User::factory()->create();
+        $this -> be($user);
+        
+        $response = $this->postJson('/board', [
             'title' => 'My Board',
             'details' => 'Something about my board.',
 
