bc51a270099be2f94f8e3a2a8992efb63d20928a
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-05-17 08:27:26 +0330

    refactor (board) : move the validation from web to controller method store

---
+++ app/Http/Controllers/BoardController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Board;
+use Illuminate\Http\Request;
+
+class BoardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title' => 'required|min:3',
+            'details' => 'nullable',
+        ]);
+        Board::create($validated);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
