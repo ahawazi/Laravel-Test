8f96d90929830c477a0b4192b035ebaf62de98ac
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-05-17 17:11:33 +0330

    feat (Board) : add the new relation between the board and user

--- app/Models/Board.php
+++ app/Models/Board.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Board extends Model
 {
@@ -13,4 +14,9 @@ class Board extends Model
         'title',
         'details',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
